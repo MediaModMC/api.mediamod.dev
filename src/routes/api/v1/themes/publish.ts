9c0f049e7136c477fe6c427101eddffbd12c454d
Conor Byrne <71222289+cbyrneee@users.noreply.github.com>
2022-04-16 14:46:44 +0100

    feat(themes/publish): Use the Mojang session server to validate requests
    The client sends us a username and a shared secret in the body of the
    request, we then combine this hash with a shared constant to get the
    actual server ID hash that was sent to the login endpoint.
    
    We can then use this hash to ensure that the request has at least came
    from an actual Minecraft account, and not someone trying to spam our
    API with bogus requests.

--- src/routes/api/v1/themes/publish.ts
+++ src/routes/api/v1/themes/publish.ts
@@ -1,9 +1,21 @@
 import { FastifyRequest } from "fastify"
+import { sessionHasJoined } from "../../../../lib/mojang"
+import { getServerIdHash } from "../../../../lib/hash"
 
 interface PublishQuery {
-    // TODO
+    username: string
+    sharedSecret: string
+
+    // TODO: Theme data
 }
 
 export async function themesPublishRoute(request: FastifyRequest<{ Body: PublishQuery }>) {
+    const serverIdHash = getServerIdHash(request.body.sharedSecret)
+    const result = await sessionHasJoined(request.body.username, serverIdHash)
+
+    if (!result.ok) {
+        return { ok: false, message: result.error }
+    }
+
     return { ok: true }
 }
